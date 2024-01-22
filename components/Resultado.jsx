180903c6e247f9cbec89ae905f8d3140928f44c1
EmilyMontalvo25 <emily.montalvo@epn.edu.ec>
2024-01-22 01:38:30 -0500

    improvements

--- components/Resultado.jsx
+++ components/Resultado.jsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
+const Contenedor = styled.div`
+  color: #fff;
+  font-family:  'Lato', sans-serif;
+   display: flex;
+   align-items: center;
+   gap: 1rem;
+   margin-top: 30px;
+`
+const Imagen = styled.img`
+  display:block;
+  width: 120px;
+`
+const Texto = styled.p`
+  font-size: 18px;
+  span{
+    font-weight: 700;
+  }
+`
+const Precio = styled.p`
+  font-size: 24px;
+  span{
+    font-weight: 700;
+  }
+
+`
+
 const Resultado = ({resultado}) => {
     const { PRICE, HIGHDAY, LOWDAY,CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado 
   
  return (
-    <div>
-      <p>El precio es de: <span>{PRICE}</span> </p>
-      <p>Precio mas alto del dia: <span>{HIGHDAY}</span> </p>
-      <p>Precio mas bajo del dia: <span>{LOWDAY}</span> </p>
-      <p>Variacio ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </p>
-      <p>Ultima Actualizacion: <span>{LASTUPDATE}</span> </p> 
-    
-
-    </div>
+    <Contenedor>
+      <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="imagenCripto" />
+      <div>
+      <Precio>El precio es de: <span>{PRICE}</span> </Precio>
+      <Texto>Precio mas alto del dia: <span>{HIGHDAY}</span> </Texto>
+      <Texto>Precio mas bajo del dia: <span>{LOWDAY}</span> </Texto>
+      <Texto>Variacio ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </Texto>
+      <Texto>Ultima Actualizacion: <span>{LASTUPDATE}</span> </Texto> 
+      </div>  
+    </Contenedor>
   )
 }
 
